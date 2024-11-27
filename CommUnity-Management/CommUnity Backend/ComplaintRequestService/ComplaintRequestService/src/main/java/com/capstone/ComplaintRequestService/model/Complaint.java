16a7b9b14cadbaa5df69ddad9246ea249489a294
Tharunkumar-07 <tharunreigns@gmail.com>
2024-11-27 11:37:50 +0530

    Initial commit with Community project

---
+++ CommUnity-Management/CommUnity Backend/ComplaintRequestService/ComplaintRequestService/src/main/java/com/capstone/ComplaintRequestService/model/Complaint.java
@@ -0,0 +1,27 @@
+package com.capstone.ComplaintRequestService.model;
+
+import com.capstone.ComplaintRequestService.ComplaintStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Complaint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long complaintId;
+    private String personName;
+    private String flatNo;
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private ComplaintStatus status = ComplaintStatus.OPEN; // Default status is OPE
+    private long residentId;
+    private long societyId;
+}
